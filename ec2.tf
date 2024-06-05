61ac4f8c7f584bfd69090a0c1aeeca6f401b548f
PandaCloud <134182273+pandacloud1@users.noreply.github.com>
2024-06-05 09:56:14 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,5 @@
-#STEP1: DEFINE AWS VERSION
+# NOTE: Save the file with extension '.tf'
+# STEP1: DEFINE AWS VERSION
 terraform {
   required_providers {
     aws = {
@@ -8,12 +9,12 @@ terraform {
   }
 }
 
-#STEP2: DEFINE AWS REGION
+# STEP2: DEFINE AWS REGION
 provider "aws" {
   region = "us-east-1"
 }
 
-#STEP3: CREATE EC2 INSTANCE
+# STEP3: CREATE EC2 INSTANCE
 resource "aws_instance" "My_Server" {
   ami           = "ami-0d191299f2822b1fa"
   instance_type = "t2.micro"
